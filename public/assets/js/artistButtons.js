ae909fd9db7544af016691558e7a64c2caf56e16
Cornell <corvinforeman@gmail.com>
2018-03-29 15:32:38 -0500

    new changes

--- public/assets/js/artistButtons.js
+++ public/assets/js/artistButtons.js
@@ -22,4 +22,23 @@
 			      }
 			    );
 			  });
+
+        	$(".remover").on("click", function(event) {
+			    var id = $(this).data("id");
+			    var signed = $(this).data("signed");
+
+			    var contractState = {
+			      taken: 0
+			    };
+			    $.ajax("/artist/" + id, {
+			      type: "PUT",
+			      data: contractState
+			    }).then(
+			      function() {
+			        console.log("changed artist contract to", signed);
+			        // Reload the page to get the updated list
+			        location.reload();
+			      }
+			    );
+			  });
        
